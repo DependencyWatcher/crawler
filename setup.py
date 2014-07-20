41a981b6ef91b359680ff862a5f2e3953bdea5fe
Michael Spector <spektom@gmail.com>
2014-07-20 15:49:40 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+	name='DependencyWatcher Crawler',
+	version='1.0',
+	url='http://github.com/DependencyWatcher/crawler/',
+	author='Michael Spector',
+	license='Apache 2.0',
+	author_email='spektom@gmail.com',
+	long_description=__doc__,
+	packages=find_packages(),
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		'lxml'
+	]
+)
