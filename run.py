2b0147a1a2f3ba3d4b0738e8cb42c79379a9efa5
Michael Spector <spektom@gmail.com>
2014-08-05 16:52:12 +0300

    multithreaded crawler

--- run.py
+++ run.py
@@ -6,16 +6,25 @@ sys.dont_write_bytecode = True
 
 from dependencywatcher.crawler.updates import *
 from dependencywatcher.crawler.manifest import *
+from multiprocessing.pool import ThreadPool
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
 # Test: iterate on all manifests, resolve updates and print them:
 import pprint
+def print_result(update):
+	pprint.pprint(update)
+
+pool = ThreadPool(processes=10)
+
 for m in FileManifestLoader().load_all():
-	try:
-		update = UpdateFinder().find_update(m)
-		pprint.pprint(update)
-	except Exception as e:
-		logging.exception("Couldn't find update for: %s" % m["name"])
+	pool.apply_async(UpdateFinder().find_update, args = (m,), callback = print_result)
+
+pool.close()
+pool.join()
+
+	#try:
+	#except Exception as e:
+	#	logging.exception("Couldn't find update for: %s" % m["name"])
 
